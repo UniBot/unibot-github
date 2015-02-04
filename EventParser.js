00d9d348cb8e2c5af2453965604a9a4292411c4c
tarlepp <tarmo.leppanen@kase.fi>
2015-02-04 21:23:41 +0200

    Added support for 'CreateEvent' and 'PullRequestEvent' also added fallback for short url generation.

--- EventParser.js
+++ EventParser.js
@@ -1,8 +1,16 @@
 'use strict';
 
 var shorturl = require('shorturl');
+var _ = require('lodash');
 
 module.exports = {
+    'CreateEvent': function parser(event, next) {
+        this._makeShortUrl(
+            ['Created new repository', event.repo.name, event.payload.description],
+            'https://github.com/' + event.repo.name,
+            next
+        );
+    },
     'IssuesEvent': function parser(event, next) {
         this._makeShortUrl(
             [event.payload.action + ' issue', event.repo.name],
@@ -14,6 +22,13 @@ module.exports = {
         this._makeShortUrl(
             ['Commented issue', event.repo.name],
             event.payload.comment.html_url,
+            next
+        );
+    },
+    'PullRequestEvent': function parser(event, next) {
+        this._makeShortUrl(
+            [event.payload.action + ' pull request', event.repo.name],
+            event.payload.pull_request.html_url,
             next
         );
     },
@@ -31,11 +46,21 @@ module.exports = {
             next
         );
     },
-    '_makeShortUrl': function _makeShortUrl(message, url, next) {
-        shorturl(url, function done(shortUrl) {
-            message.push(shortUrl);
+    '_makeShortUrl': function _makeShortUrl(message, url, next, services) {
+        var self = this;
 
-            next(null, message.join(' - '));
+        services = services || ['arseh.at', 'goo.gl', 'is.gd', 'v.gd'];
+
+        var service = services.pop();
+
+        shorturl(url, service, function done(shortUrl) {
+            if (_.isUndefined(shortUrl) && services.length > 0) {
+                self._makeShortUrl(message, url, next, services);
+            } else {
+                message.push(shortUrl || url);
+
+                next(null, message.join(' - '));
+            }
         });
     }
 };
